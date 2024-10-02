935c2aebbfae71c6a31e26f5769adde7a60f99f0
edw0rd21 <www.srijitmondal210@gmail.com>
2024-10-02 21:59:58 +0530

    added camera class, and updated main file to create a chunk for minecraft world

--- src/opengl.cpp
+++ src/opengl.cpp
@@ -9,36 +9,79 @@
 #include "stb_image.h"
 
 #include "Shader.h"
+#include "camera.h"
 
+#include <vector>
 #include <iostream>
 
+const unsigned int SCR_WIDTH = 1920;
+const unsigned int SCR_HEIGHT = 1080;
+
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+    
+float deltaTime = 0.0f;	
+float lastFrame = 0.0f;
+
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; 
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
 
 void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);   
 
-    static bool wireframe = true;
-
-    if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS)
-    {
+    if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS) 
         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    }
- 
+  
     else if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
-    {
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // Set to filled
-    }
 
+    float cameraSpeed = static_cast<float>(2.5 * deltaTime);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) 
+        camera.ProcessKeyboard(UP, 4 * deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) 
+        camera.ProcessKeyboard(DOWN, 4 * deltaTime);
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
-
-const unsigned int SCR_WIDTH = 1920;
-const unsigned int SCR_HEIGHT = 1080;
 
 int main()
 {
@@ -51,7 +94,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, ":)", glfwGetPrimaryMonitor(), NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, ":)", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -59,8 +102,11 @@ int main()
         return -1;
     }
     glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
+    glfwSetFramebufferSizeCallback(window, &framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, &mouse_callback);
+    glfwSetScrollCallback(window, &scroll_callback);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+        
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
@@ -114,19 +160,6 @@ int main()
         -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
-    
-    glm::vec3 cubePositions[] = {
-        glm::vec3(0.0f,  0.0f,  0.0f),
-        glm::vec3(2.0f,  5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3(2.4f, -0.4f, -3.5f),
-        glm::vec3(-1.7f,  3.0f, -7.5f),
-        glm::vec3(1.3f, -2.0f, -2.5f),
-        glm::vec3(1.5f,  2.0f, -2.5f),
-        glm::vec3(1.5f,  0.2f, -1.5f),
-        glm::vec3(-1.3f,  1.0f, -1.5f)
-    };
 
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -163,7 +196,7 @@ int main()
 
     stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
     
-    data = stbi_load("textures/ganj.png", &width, &height, &nrChannels, 0);
+    data = stbi_load("textures/grass-block.png", &width, &height, &nrChannels, 0);
     if (data)
     {
         if (nrChannels == 4)  // For PNG files with RGBA channels
@@ -188,11 +221,15 @@ int main()
     // render loop
     while (!glfwWindowShouldClose(window))
     {
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+            
         // input
         processInput(window);
 
         // render
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.5f, 0.7f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glActiveTexture(GL_TEXTURE0);
@@ -200,29 +237,46 @@ int main()
 
         ourShader.use();
 
-        // create transformations
-        glm::mat4 projection = glm::mat4(1.0f);
-        glm::mat4 view = glm::mat4(1.0f);
-
-        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.f);
-        view= glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.f);
         ourShader.setMat4("projection", projection);
+            
+        glm::mat4 view = camera.GetViewMatrix();
         ourShader.setMat4("view", view);
 
         glBindVertexArray(VAO);
 
-        for (unsigned int i = 0; i < 10; i++)
+        const int CHUNK_SIZE = 50; // Define the size of the chunk
+
+        std::vector<glm::vec3> cubePositions;
+
+        for (int x = 0; x < CHUNK_SIZE; ++x) {
+            for (int y = 0; y < 2; ++y) {
+                for (int z = 0; z < CHUNK_SIZE; ++z) {
+                    // Calculate the position for each cube
+                    glm::vec3 position = glm::vec3(x, y, z);
+                    cubePositions.push_back(position);
+                }
+            }
+        }
+
+        for (const auto& position : cubePositions)
         {
-            // calculate the model matrix for each object and pass it to shader before drawing
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, cubePositions[i]);
-            float angle = glfwGetTime() * 50.0f + (30.0f * i);
-            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), position);
             ourShader.setMat4("model", model);
 
-            glDrawArrays(GL_TRIANGLES, 0, 36);
+            glDrawArrays(GL_TRIANGLES, 0, 36); 
         }
+
+
+        //for (unsigned int i = 0; i < 10; i++)
+        //{
+           /* glm::mat4 model = glm::translate(glm::mat4(1.0f), cubePositions[0]);
+            float angle = glfwGetTime() * 50.0f + (30.0f * 0);
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            ourShader.setMat4("model", model);*/
+
+            //glDrawArrays(GL_TRIANGLES, 0, 36);
+        //}
 
         glfwSwapBuffers(window);
         glfwPollEvents();
